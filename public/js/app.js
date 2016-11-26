e5008faa16d2a2a4821ada8961e9105058d417a5
Wasif Ateeq <wasifateeq007@hotmail.com>
2016-11-26 13:34:21 -0800

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ socket.on('connect',function()
 {
 console.log("User connected to server");
 });
+
+socket.on("message",function(message){
+console.log("New message");
+console.log(message.type);
+});
--- server.js
+++ server.js
@@ -4,8 +4,19 @@ var app=express();
 var http=require('http').Server(app);
 var io=require('socket.io')(http);
 app.use(express.static(__dirname+"/public"));
-io.on("connection",function(){
+
+io.on("connection",function(socket){
 console.log("Connected via socket.io");
+
+
+socket.emit("message",{
+	type:"Message Send from server to client"
+});
+
+socket.on("message",function(message){
+	console.log("message recieved :"+message.type);
+	socket.broadcast.emit("message",message);
+});
 });
 http.listen(PORT,function(){
 	console.log("Server started");
