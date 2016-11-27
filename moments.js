b6e4e19ec8814a7b1e81a4c9b4929c5961cb5d6e
Wasif Ateeq <wasifateeq007@hotmail.com>
2016-11-26 23:22:17 -0800

    Using utc timestamp for getting current time

--- moments.js
+++ moments.js
@@ -2,3 +2,9 @@ var moment=require('moment');
 
 var now=moment();
 console.log(now.format('MMM Do YYYY, h:mma'));
+
+console.log(now.format('x'));
+var val=1480230803873;
+var time=moment.utc(val);
+
+console.log(time.local().format("h:mm a"));
